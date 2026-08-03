ce299e9c066d2eb032f4e422810dda1c8371b314
K. Vignesh <192472154.simats@saveetha.com>
2026-08-03 13:28:26 +0530

    ADD

--- C01-AT/LAB EXP/nlp lab exp-01.py
+++
@@ -1,9 +0,0 @@
-import re
-text = input("enter the text: ")
-pattern = input("enter pattern to search: ")
-match = re.search(pattern,text)
-if match:
-    print("pattern found")
-    print(str(match.start()) +" "+ str(match.end()))
-else:
-    print("pattern not found")
